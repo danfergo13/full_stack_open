0f5b5f8095327f4b2558ac92369754464fe71fe7
Daniel Gómez Díaz <danmercad@gmail.com>
2025-05-02 16:55:34 -0400

    1.7: unicafe, paso 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,10 +4,14 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
+
   const Button = (props) => (
     <button onClick={props.handleClick}>{props.text}</button>
   );
+
+  const total = good + neutral + bad;
+  const promedio = total > 0 ? (good * 1 + bad * -1) / total : 0;
+  const porcentajePositivo = total > 0 ? (good / total) * 100 : 0;
 
   return (
     <div>
@@ -15,14 +19,22 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      
+
       <h1>statistics</h1>
+
       <div>
         good {good}
         <br />
         neutral {neutral}
         <br />
         bad {bad}
+        <br />
+        all {total}
+        <br />
+        average {promedio}
+        <br />
+        positive {porcentajePositivo} %
+        <br />
       </div>
     </div>
   );
